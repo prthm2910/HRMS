b319f59486b2ca27009a2eb4fb99954d063d83db
prthm2910 <pratham.kubasad2004@gmail.com>
2026-01-09 11:20:55 +0530

    add customization

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,5 @@
+# hrms/apps/users/admin.py
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
@@ -5,17 +7,20 @@ from .models import User
 class CustomUserAdmin(UserAdmin):
     model = User
     
-    # 1. Display these fields in the list view (the table of all users)
+    # 1. List View Customization
+    # Controls columns in the user list table.
+    # Added 'phone_number' which isn't there by default.
     list_display = ['username', 'email', 'phone_number', 'is_staff']
 
-    # 2. Add your custom fields to the "Edit User" page
-    # We append a new section called 'Additional Info' to the existing fieldsets
+    # 2. Edit Page Customization (Fieldsets)
+    # Django's default UserAdmin doesn't know about 'bio' or 'phone_number'.
+    # We append a new section "Additional Info" to the existing form.
     fieldsets = list(UserAdmin.fieldsets) + [
         ('Additional Info', {'fields': ('phone_number', 'bio')}),
     ]
 
-    # 3. Add your custom fields to the "Add User" page
-    # This controls what you see when you click "Add User" in the admin
+    # 3. Add Page Customization
+    # When creating a NEW user, we also want to see these fields.
     add_fieldsets = list(UserAdmin.add_fieldsets) + [
         ('Additional Info', {'fields': ('phone_number', 'bio')}),
     ]
