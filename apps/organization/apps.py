72b06038095b590164991e35644d8c8b7064dc1e
prthm2910 <pratham.kubasad2004@gmail.com>
2026-01-15 15:37:31 +0530

    refactor : relative import paths --> absolute import paths

--- apps/organization/apps.py
+++ apps/organization/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 class OrganizationConfig(AppConfig):  # Renamed from EmployeesConfig
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'organization'             # Renamed from 'employees'
+    name = 'apps.organization'             # Renamed from 'employees'
+    label = 'organization'  # App label for model references
