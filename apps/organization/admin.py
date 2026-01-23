92061ad561d012c5c5ce1440ede7cb8191acd461
prthm2910 <pratham.kubasad2004@gmail.com>
2026-01-23 10:06:17 +0530

    customize Employee model list_display

--- apps/organization/admin.py
+++ apps/organization/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from apps.organization.models import Employee, Department
 # Register your models here.
 
-admin.site.register(Employee)
+# admin.site.register(Employee)
 admin.site.register(Department)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'employee_id', 'department', 'designation', 'employment_type', 'date_of_joining')
