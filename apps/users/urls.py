f58a0340db8829c7490961dcd9f17577fd17afec
prthm2910 <pratham.kubasad2004@gmail.com>
2026-01-27 10:12:17 +0530

    change profile/ endpoint API view

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import (
      TokenObtainPairView,
      TokenRefreshView,
 )
-from apps.users.views import RegisterView, UserProfileView
+from apps.users.views import  UserProfileView
 
 urlpatterns = [
     # Login Endpoint (Get Access + Refresh Token)
